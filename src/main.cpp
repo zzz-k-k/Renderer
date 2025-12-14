2375de07a69ded9360d3f6e1b8d0273d7c527bcb
zzz-k-k <117960486+zzz-k-k@users.noreply.github.com>
2025-12-14 12:27:38 +0800

    更新了着色器和独立的shader

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,9 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include<shader.h>
 #include<iostream>
-const char *vertexShaderSource = "#version 330 core\n"
-    "layout (location = 0) in vec3 aPos;\n"
-    "void main()\n"
-    "{\n"
-    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-    "}\0";
+#include<cmath>
 
-const char *fragmentShaderSource="#version 330 core\n"
-    "out vec4 FragColor;\n"
-    "uniform vec4 uColor;\n"
-    "void main()\n"
-    "{\n"
-    "   FragColor = uColor;\n"
-    "}\0";
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
@@ -45,19 +34,9 @@ int main()
 
     float vertices[] = 
     {
-        0.5f, 0.5f, 0.0f,   // 右上角
-        0.5f, -0.5f, 0.0f,  // 右下角
-        -0.5f, -0.5f, 0.0f, // 左下角
-        -0.5f, 0.5f, 0.0f, 
-        
-        -0.8f,0.0f,0.0f,
-        0.8f,0.0f,0.0f,
-        0.0f,0.8f,0.0f,
-
-        -0.8f,-0.1f,0.0f,
-        0.8f,-0.1f,0.0f,
-        0.0f,-0.8f,0.0f
-
+        -0.8f,0.0f,0.0f,1.0f,0.0f,0.0f,
+        0.8f,0.0f,0.0f,0.0f,1.0f,0.0f,
+        0.0f,0.8f,0.0f,0.0f,0.0f,1.0f
     };
 
     unsigned int indices[]=
@@ -66,48 +45,14 @@ int main()
         1,2,3
     };
     
-    //动态编译着色器
-    unsigned int vertexShader;
-    vertexShader=glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader,1,&vertexShaderSource,NULL);
-    glCompileShader(vertexShader);
-    
-    //检查shader是否编译成功
-    int  success;
-    char infoLog[512];
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-    if(!success)
-    {
-        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-    }
-    
-    //片段着色器
-    unsigned int fragmentShader;
-    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-    glCompileShader(fragmentShader);
-
-    //程序对象
-    unsigned int shaderProgram;
-    shaderProgram=glCreateProgram();
-    glAttachShader(shaderProgram,vertexShader);
-    glAttachShader(shaderProgram,fragmentShader);
-    glLinkProgram(shaderProgram);
-
-    int colorLoc=glGetUniformLocation(shaderProgram,"uColor");
-
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
 
     //使用顶点数组对象
-    unsigned int VAO_sqr,VBO,EBO,VAO_tri;
-    glGenVertexArrays(1, &VAO_sqr);
-    glGenVertexArrays(1,&VAO_tri);
+    unsigned int VAO,VBO,EBO;
+    glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
     glGenBuffers(1,&EBO);
     // 1. 绑定VAO_sqr
-    glBindVertexArray(VAO_sqr);
+    glBindVertexArray(VAO);
     // 2. 把顶点数组复制到缓冲中供OpenGL使用
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
@@ -116,37 +61,30 @@ int main()
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     // 3. 设置顶点属性指针
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
-    
-    //绑定VAO_tri
-    glBindVertexArray(VAO_tri);
-    glBindBuffer(GL_ARRAY_BUFFER,VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
+    //设置颜色
+    glVertexAttribPointer(1,3,GL_FLOAT,GL_FALSE,6*sizeof(float),(void*)(3*sizeof(float)));
+    glEnableVertexAttribArray(1);
+    
     
     glViewport(0, 0, 800, 600);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    Shader ourShader("../../src/shader/shader.vs", "../../src/shader/shader.fs");
+
     while(!glfwWindowShouldClose(window))
     {
         processInput(window);
         
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
-        
-        glUseProgram(shaderProgram);
-        glUniform4f(colorLoc, 1.0f, 0.5f, 0.2f, 1.0f);
-        glBindVertexArray(VAO_sqr);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-        
-        //也可以选择去掉注释
-        // glBindVertexArray(0);
-        // glBindVertexArray(VAO_tri);
-        glUniform4f(colorLoc, 0.2f, 0.8f, 1.0f, 1.0f);
-        glDrawArrays(GL_TRIANGLES,4,3);
-        glDrawArrays(GL_TRIANGLES,7,3);
+
+
+        ourShader.use();
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES,0,3);
 
 
         
