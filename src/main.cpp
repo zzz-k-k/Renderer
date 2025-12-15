aa2681906b88f15b46015c51a7a716d174213d65
zzz-k-k <117960486+zzz-k-k@users.noreply.github.com>
2025-12-15 13:18:06 +0800

    添加了纹理

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,12 @@
 #include<shader.h>
 #include<iostream>
 #include<cmath>
+#include<stb_image.h>
 
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
+unsigned int LoadTexture2D(const char* path);
 
 int main()
 {
@@ -34,9 +36,10 @@ int main()
 
     float vertices[] = 
     {
-        -0.8f,0.0f,0.0f,1.0f,0.0f,0.0f,
-        0.8f,0.0f,0.0f,0.0f,1.0f,0.0f,
-        0.0f,0.8f,0.0f,0.0f,0.0f,1.0f
+        0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // 右上
+        0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // 右下
+        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // 左下
+        -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // 左上
     };
 
     unsigned int indices[]=
@@ -45,7 +48,12 @@ int main()
         1,2,3
     };
     
+    //------------创建生成纹理对象---------
+    unsigned int texture1 = LoadTexture2D("container.jpg");
+    unsigned int texture2 = LoadTexture2D("awesomeface.png");
 
+
+    //-----------创建顶点数组对象------------
     //使用顶点数组对象
     unsigned int VAO,VBO,EBO;
     glGenVertexArrays(1, &VAO);
@@ -61,18 +69,26 @@ int main()
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     // 3. 设置顶点属性指针
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
     //设置颜色
-    glVertexAttribPointer(1,3,GL_FLOAT,GL_FALSE,6*sizeof(float),(void*)(3*sizeof(float)));
+    glVertexAttribPointer(1,3,GL_FLOAT,GL_FALSE,8*sizeof(float),(void*)(3*sizeof(float)));
     glEnableVertexAttribArray(1);
+
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
     
     
     glViewport(0, 0, 800, 600);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
     Shader ourShader("../../src/shader/shader.vs", "../../src/shader/shader.fs");
+    ourShader.use();
+
+
+    glUniform1i(glGetUniformLocation(ourShader.ID, "texture1"), 0); // 手动设置
+    ourShader.setInt("texture2", 1); // 或者使用着色器类设置
 
     while(!glfwWindowShouldClose(window))
     {
@@ -82,10 +98,13 @@ int main()
         glClear(GL_COLOR_BUFFER_BIT);
 
 
-        ourShader.use();
-        glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES,0,3);
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture1);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, texture2);
 
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         
         glfwSwapBuffers(window);//显示窗口
@@ -106,4 +125,35 @@ void processInput(GLFWwindow *window)
 {
     if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
+}
+
+//加载纹理对象
+unsigned int LoadTexture2D(const char* path)
+{
+    unsigned int tex;
+    glGenTextures(1, &tex);
+    glBindTexture(GL_TEXTURE_2D, tex);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    int w, h, c;
+    stbi_set_flip_vertically_on_load(true);
+    unsigned char* data = stbi_load(path, &w, &h, &c, 0);
+    if (data)
+    {
+        GLenum format = (c == 4) ? GL_RGBA : GL_RGB;
+        glTexImage2D(GL_TEXTURE_2D, 0, format, w, h, 0, format, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture: " << path
+                  << " reason: " << stbi_failure_reason() << std::endl;
+    }
+    stbi_image_free(data);
+
+    return tex;
 }
